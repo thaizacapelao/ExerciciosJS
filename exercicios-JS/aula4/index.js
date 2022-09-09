d50bcbe8a4fe319eba74eb10d25e5eade5379927
Thaiza Capelão <104083691+thaizacapelao@users.noreply.github.com>
2022-09-09 13:41:28 -0400

    Anexando arquivos iniciais

---
+++ exercicios-JS/aula4/index.js
@@ -0,0 +1,9 @@
+//Exercicio 1 (Aula 8 Udemy):
+
+console.log('Meu nome é "Thaiza". Estou aprendendo JavaScript às', 10 ,'da manhã.');
+console.log('Este', 'É', 'Um', 1000);
+
+/*Exercicio 2:
+$ ssh-keygen -t rsa -b 4096 -C "thaizacapelao@gmail.com"
+*/
+
