fc9c972d28587605e562d9bbec52435906640eaf
Thaiza Capelão <thaizacapelao@gmail.com>
2022-12-28 14:06:30 -0400

    Adicionando a aula 34 de funções, array e objeto

---
+++ exercicios-JS/aula20/js/script.js
@@ -0,0 +1,36 @@
+function meuEscopo () {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
+    // form.onsubmit = function (evento) {
+    //     alert(1);
+    //     console.log('Foi enviado.');
+    // }; 
+    // 1- Uma maneira de enviar o formulario.
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ` + 
+        `${peso.value} ${altura.value} </p>`;
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+}
+
+meuEscopo();
