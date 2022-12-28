f507fe9f5e7accb2c5622160dab30d29bae746a9
Thaiza Capelão <thaizacapelao@gmail.com>
2022-12-28 15:17:10 -0400

    Adicionando aula 21 de JS -  Operadores Lógicos

---
+++ exercicios-JS/aula21/index.js
@@ -0,0 +1,21 @@
+/*
+Operadores de comparação
+1- > maior que
+2- >= maior que ou igual a
+3- < menor que
+4- <= menor que ou igual a
+5- == igualdade (checa valor) ******
+6- === igualdade estrita (checa valor e tipo)
+7- != diferente (valor) *******
+8- !== diferente estrito (valor e tipo)
+*/
+
+// const num1 = 10; // number
+// const num2 = '10'; //string
+// const expressao = num1 === num2;
+// console.log(expressao);
+
+const num1 = 10; // number
+const num2 = '10'; //string
+const expressao = num1 !== num2;
+console.log(expressao);
