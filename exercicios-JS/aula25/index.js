34c26182c6a35764f6989e913322f0df00018b14
Thaiza Capelão <thaizacapelao@gmail.com>
2023-01-07 11:50:50 -0400

    Adicionando aula 25 - if, else if e else (2)

---
+++ exercicios-JS/aula25/index.js
@@ -0,0 +1,14 @@
+const numero = 6;
+
+// Se ocorrer (numero >= 0 && numero <=5), faça isso {código} 
+// Se não {o código}
+
+if (numero >= 0 && numero <=5) {
+    console.log('Sim, o número está entre 0 e 5.');
+} else if (numero >=6 && numero <=8) {
+    console.log('Não, o número não esta entre 6 e 8.');
+} else if (numero >=9 && numero <=11) {
+    console.log('Não, o número não esta entre 9 e 11.');
+} else {
+    console.log('O número não esta entre 0 e 11.');
+}
